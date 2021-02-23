17ad366fc48d83f0d95eceb0d7a705e80fa2c7cc
Pedro Menegussi <pedrovipmenegussi@gmail.com>
2021-02-23 17:15:14 -0300

    sequelize add

--- _javascript/index.js
+++ _javascript/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const BodyParser = require("body-parser")
+const Sequelize = require("sequelize")
 
 app.listen(8089, function(){
     const port = "8089"
@@ -19,8 +21,23 @@ app.get("/cotacoes/:chamado/:item/:descricao/:valor", function(req, res){
 });
 
 app.post("/pegaformulario", function(req, res){
-    res.send("Data: "+req.body.data_pc+ "Processo de Compra: "+req.body.p_c)
+    res.send("Data do processo de compra: "+req.body.data_pc)
 });
 
 app.use(express.static('public'));
 
+//BodyParser
+app.use(BodyParser.urlencoded({extended:false}))
+app.use(BodyParser.json())
+
+//Banco de dados
+const sequelize = new Sequelize('logistica', 'root', '27452121', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function(){
+    console.log('Conectado com sucesso!')
+}).catch(function(erro){
+    console.log('falha ao se conectar! '+erro)
+})
